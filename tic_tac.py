f8e60146f202f7148f3a3d70e476410ab38bd1e0
Daedpuul <Pickleman967@gmail.com>
2019-03-30 17:11:16 +0300

    Subscribe to CallmeCarson

---
+++ tic_tac.py
@@ -0,0 +1,8 @@
+array= [0,1,2,12,5]
+
+def print_index_times2(array,index ):
+    print(array[index] * 2)
+print_index_times2(array,2)
+def print_by_value(array,value):
+    print(array[array.index(value)])
+print_by_value(array,5)
