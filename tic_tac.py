3ed1c30f54b1951d7c7ed4a9c9e7a45d1e55d398
adammcpherson <aomcpherson@gmail.com>
2019-03-30 09:53:29 -0500

    Added game_board and start of move function

--- tic_tac.py
+++ tic_tac.py
@@ -1,4 +1,14 @@
-array= [0,1,2,12,5]
+array = [0,1,2,12,5]
+game_board = [['q','q','q'],
+              ['q','q','q'],
+              ['q','q','q']]
+
+def move(player, row, column):
+    game_board[row][column] = player
+
+move('X',0,0)
+move('Y',0,0)
+print(game_board)
 
 def print_index_times2(array,index ):
     print(array[index] * 2)
@@ -7,5 +17,6 @@ def print_by_value(array,value):
     print(array[array.index(value)])
 print_by_value(array,5)
 
-# Hey Isaac! 
+# Hey Isaac!
+
 
